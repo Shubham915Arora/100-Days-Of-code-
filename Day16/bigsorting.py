b7bcc54f0e8750b1e4fd9c43476b63aec9010d58
Shubham915Arora <500061938@stu.upes.ac.in>
2020-07-18 14:07:10 +0530

    16th Day 3rd Commit

---
+++ Day16/bigsorting.py
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the bigSorting function below.
+vector<string> bigSorting(vector<string> unsorted) {
+    int min=0;
+    for(int =0;i<unsorted.size();i++)
+    {
+        min=
+    }
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> unsorted(n);
+
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
+
+        unsorted[i] = unsorted_item;
+    }
+
+    vector<string> result = bigSorting(unsorted);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
